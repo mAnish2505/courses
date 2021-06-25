0a2109a9766785e0d05161f5544cdaa76e37ac92
Manish Kumar <kiet.ecmanish@example.com>
2021-06-25 15:26:25 +0530

    Initial commit

---
+++ src/main/java/springboot/starter/topic/courses/SpringBootTopicsApplication.java
@@ -0,0 +1,13 @@
+package springboot.starter.topic.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTopicsApplication.class, args);
+	}
+
+}
